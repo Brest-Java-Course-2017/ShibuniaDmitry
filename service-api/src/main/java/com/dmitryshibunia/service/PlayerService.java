5d11e3bd62c2a48512a6c4dc262ad928c1fbf7f2
DmitryShibunia <dimgo_gamer@mail.ru>
2017-03-19 17:09:58 +0300

    add service interface

---
+++ service-api/src/main/java/com/dmitryshibunia/service/PlayerService.java
@@ -0,0 +1,70 @@
+package com.dmitryshibunia.service;
+
+import com.dmitryshibunia.model.Player;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface PlayerService {
+
+    /**
+     * Get all players list
+     *
+     * @param teamId team identifier
+     *
+     * @return all players list
+     */
+    List<Player> getAllPlayersInTeam(Integer teamId);
+
+    /**
+     * Get player by id
+     *
+     * @param id player's id
+     *
+     * @return player object
+     */
+    Player getPlayerById(Integer id);
+
+    /**
+     * add new player
+     *
+     * @param player player object
+     *
+     * @return player id
+     */
+    Integer addPlayer(Player player);
+
+    /**
+     * update player
+     *
+     * @param player player object
+     */
+    int updatePlayer(Player player);
+
+    /**
+     * delete player
+     *
+     * @param id player identifier
+     */
+    int deletePlayer(Integer id);
+
+    /**
+     * count players quantity in team
+     *
+     * @param teamId team identifier
+     *
+     * @return players quantity
+     */
+    int countPlayersInTeam(Integer teamId);
+
+    /**
+     * Get list of players filtered by acceptance date
+     *
+     * @param teamId team identifier
+     * @param from beginning of period
+     * @param to end of period
+     *
+     * @return filtered players list
+     */
+    List<Player> filterPlayersByAcceptanceDate(Integer teamId, LocalDate from, LocalDate to);
+}
---
+++ service-api/src/main/java/com/dmitryshibunia/service/TeamService.java
@@ -0,0 +1,56 @@
+package com.dmitryshibunia.service;
+
+import com.dmitryshibunia.model.Team;
+
+import java.util.List;
+
+public interface TeamService {
+
+    /**
+     * Get all teams list
+     *
+     * @return all teams list
+     */
+    List<Team> getAllTeams();
+
+    /**
+     * Get team by id
+     *
+     * @param id team identifier
+     *
+     * @return team object
+     */
+    Team getTeamById(Integer id);
+
+    /**
+     * Get team by name
+     *
+     * @param name team name
+     *
+     * @return team object
+     */
+    Team getTeamByName(String name);
+
+    /**
+     * add new team
+     *
+     * @param team team object
+     *
+     * @return team id
+     */
+    Integer addTeam(Team team);
+
+    /**
+     * update team
+     *
+     * @param team team object
+     */
+    int updateTeam(Team team);
+
+    /**
+     * delete team
+     *
+     * @param id team identifier
+     */
+    int deleteTeam(Integer id);
+}
